5848b9646afc416200f37eac1b7c43d1d6811dae
mzvic <vmontesinos2021@udec.cl>
2023-11-15 15:19:31 -0300

    sol2

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ with open("./DATOS/" + str(start_time) + ".csv", 'w') as f:
     while True:
         try:
             ds18b20 = read_temp()
-	        ds18b20_1 = read_temp1()
+            ds18b20_1 = read_temp1()
             time_now = datetime.now()
-	        print("{}, DS18B20: {}, DS18B20_1: {}".format(time_now, str(ds18b20), str(ds18b20_1)))
-	        f.write("{}, {}, {}\n".format(time_now, str(ds18b20), str(ds18b20_1)))
-	        print("------------------------------")
+            print("{}, DS18B20: {}, DS18B20_1: {}".format(time_now, str(ds18b20), str(ds18b20_1)))
+            f.write("{}, {}, {}\n".format(time_now, str(ds18b20), str(ds18b20_1)))
+            print("------------------------------")
         except KeyboardInterrupt:
-	        print("Nombre archivo: " + str(start_time))
-	        f.close()
-	        push()
-	        break
+            print("Nombre archivo: " + str(start_time))
+            f.close()
+            push()
+            break
