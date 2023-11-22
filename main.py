4e85a5c3f259a6fed2b1319bff60ba675f96f070
mzvic <vmontesinos2021@udec.cl>
2023-11-22 10:18:07 -0300

    change date to time

--- main.py
+++ main.py
@@ -9,22 +9,24 @@ os.system('modprobe w1-therm')
 import os
 from datetime import datetime
 
-start_time = datetime.now().isoformat()
+start_time_name = datetime.now().isoformat()
+start_time = datetime.now()
 
 def push():
     os.system("git add --all")
-    os.system('git commit -m "{}"'.format(start_time))
+    os.system('git commit -m "{}"'.format(start_time_name))
     os.system("git push")
 
 print("------------------------------")
-with open("./DATOS/" + str(start_time) + ".csv", 'w') as f:
+with open("./DATOS/" + str(start_time_name) + ".csv", 'w') as f:
     while True:
         try:
             ds18b20 = read_temp()
             ds18b20_1 = read_temp1()
             time_now = datetime.now()
-            print("{}, DS18B20: {}, DS18B20_1: {}".format(time_now, str(ds18b20), str(ds18b20_1)))
-            f.write("{}, {}, {}\n".format(time_now, str(ds18b20), str(ds18b20_1)))
+            
+            print("{}, DS18B20: {}, DS18B20_1: {}".format(time_now.time(), str(ds18b20), str(ds18b20_1)))
+            f.write("{}, {}, {}\n".format(time_now.time(), str(ds18b20), str(ds18b20_1)))
             print("------------------------------")
         except KeyboardInterrupt:
             print()
