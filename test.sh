2720850500bd63ddd632e8a007c0471d7076b400
mzvic <vmontesinos2021@udec.cl>
2023-11-22 10:58:49 -0300

    2023-11-22T10:58:43.110153

--- test.sh
+++ test.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-echo La fecha actual es: "$(date)"
+echo "La fecha actual es: $(date)"
 
 read -p "Deseas cambiarla? (s/n): " input
 
-if [[ "$input" == "s" ]]; then
+if [ "$input" = "s" ]; then
    echo "Introduce Fecha y Hora (yyyy-mm-dd hh:mm:ss)"
-   read fecha_usr hora_usr
-   #sudo date -s "${fecha_usr} ${hora_usr}"
-   #python main.py
-elif [[ "$input" == "n" ]]; then
-   #python main.py
-   echo "python main.py"
+   read -p "Fecha: " fecha_usr
+   read -p "Hora: " hora_usr
+   sudo date -s "${fecha_usr} ${hora_usr}"
+   python main.py
+elif [ "$input" = "n" ]; then
+   echo "Ejecutando python main.py..."
+   python main.py
 else
-   echo "The strings are different."
+   echo "Opción no válida. Por favor, selecciona 's' o 'n'."
 fi
+
