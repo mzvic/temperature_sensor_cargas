081adea07f6adddee2e522b5788ea493f731fe24
mzvic <vmontesinos2021@udec.cl>
2023-11-28 10:36:19 -0300

    update

--- conexion.py
+++ conexion.py
@@ -8,7 +8,7 @@ ssh.load_system_host_keys()
 
 start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # date -s
 
-ip_str = ping_and_ssh("192.168.0.128" , "192.168.0.200", "raspy", "raspy")
+ip_str = ping_and_ssh("192.168.0.100" , "192.168.0.200", "raspy", "raspy")
 
 ssh.connect(ip_str, username="raspy", password="raspy", allow_agent=False, look_for_keys=False)
 
